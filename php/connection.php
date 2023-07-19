4162e7d4624be84b5d484da17f04d14a86420e4a
RamaHabbal <137204591+RamaHabbal@users.noreply.github.com>
2023-07-20 01:08:51 +0300

    fix id and db name

--- php/connection.php
+++ php/connection.php
@@ -3,7 +3,7 @@
 $host = "localhost";
 $db_user = "root";
 $db_pass = null;
-$db_name = "miniproj";
+$db_name = "fullstackminiproject";
 
 $mysqli = new mysqli($host, $db_user, $db_pass, $db_name);
 if (!$mysqli) {
